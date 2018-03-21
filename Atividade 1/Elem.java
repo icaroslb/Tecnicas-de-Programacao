729d1cea8ddb4ad34d0c3d06b1dbcf65b8a29374
Ícaro Barbosa <31295743+icaroslb@users.noreply.github.com>
2018-03-21 15:27:17 -0300

    Update Elem.java

--- Atividade 1/Elem.java
+++ Atividade 1/Elem.java
@@ -16,37 +16,6 @@ class Elem{
 		this.ant=ant;
 	}
 	
-	/*void inserir(int val){
-		Elem aux=this, novo;
-		
-		while(aux.prox!=null && aux.val<val){
-			aux=aux.prox;
-		}
-			/*if(aux.val==val){
-				System.out.println("Esse número já exite no conjunto!");
-			}else{
-		if(aux.val!=val){
-			novo=new Elem(val, ant);
-			if(this==aux){
-				novo.prox=aux;
-				aux.ant=novo;
-				aux=novo;
-			}
-			if(aux.val<val){
-				novo.prox=aux.prox;
-				novo.ant=aux;
-				aux.prox=novo;
-				if(novo.prox!=null){
-					novo.prox.ant=novo;
-				}
-			}else{
-				novo.ant=aux;
-				novo.prox=aux.prox;
-				aux.prox=novo;
-			}
-		}
-	}*/
-	
 	boolean pertinencia(Object val){
 		Elem aux=this;
 		
