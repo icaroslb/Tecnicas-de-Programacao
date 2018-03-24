9bce56b74d6537fe52f15b96cbff6b8dc372e49e
Icaro SlBa <icrsbarbosa@gmail.com>
2018-03-23 22:11:53 -0300

    Correção na verificação dos elementos da Tupla

--- Atividade 1/Tupla.java
+++ Atividade 1/Tupla.java
@@ -7,7 +7,7 @@ class Tupla{
 	}
 	
 	public boolean equals(Object obj){
-			if(this.getClass() == obj.getClass() && (this.x==((Tupla)obj).x) && (this.y==((Tupla)obj).y)){
+			if(this.getClass() == obj.getClass() && ((Tupla)obj).x.equals(this.x) && ((Tupla)obj).y.equals(this.y)){
 				return true;
 			}else{
 				return false;
